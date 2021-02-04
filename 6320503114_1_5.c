e3fbd293a73d31c07c9eecf960651a0699fc89a7
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-04 16:35:05 +0700

    input

--- 6320503114_1_5.c
+++ 6320503114_1_5.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,i,max,lo;
     scanf("%d",&n);
+    int num[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&num[i]);
+    max=num[0];
+
 
 }
 
