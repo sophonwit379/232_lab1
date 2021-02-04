d8825ec673fb240fe2f1ea6f753573f7f6d832e8
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-04 15:54:46 +0700

    fix

--- 6320503114_1_2.c
+++ 6320503114_1_2.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
     int a,b;
     scanf("%d",&a);
-    if(0<=a && a<=pow(10,9))
-    {
-        scanf("%d",&b);
-        if(0<=b && b<=pow(10,9))
-            printf("");
-    }
-
-
-
+    scanf("%d",&b);
+    printf("%d",a+b);
 
 }
