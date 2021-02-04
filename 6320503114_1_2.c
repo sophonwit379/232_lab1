4b5b63e60c2d0a8322c7c8c0fe0dc71e78cd625a
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-04 15:53:13 +0700

    input a b

--- 6320503114_1_2.c
+++ 6320503114_1_2.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int a,b;
-    scanf("%d %d",&a,&b);
+    scanf("%d",&a);
+    if(0<=a && a<=pow(10,9))
+    {
+        scanf("%d",&b);
+        if(0<=b && b<=pow(10,9))
+            printf("");
+    }
+
 
 
 
